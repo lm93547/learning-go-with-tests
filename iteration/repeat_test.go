9f516db3308e53f1c63c4a0c30a18810d5ed890b
Lucas Martin <lucasmartin045@gmail.com>
2022-10-30 23:26:43 +0800

    updating repeat test to take arg & updating test results & adding example func

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,37 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("5 iterations", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("12 iterations", func(t *testing.T) {
+		repeated := Repeat("a", 12)
+		expected := "aaaaaaaaaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeatedString := Repeat("b", 5)
+	fmt.Println(repeatedString)
+	// Output: bbbbb
 }
