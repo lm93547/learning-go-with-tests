ce288643340ac996c38d199a17bc9c7bf7b06ca1
Lucas Martin <lucasmartin045@gmail.com>
2022-10-30 22:44:13 +0800

    adding french test case

--- hello_test.go
+++ hello_test.go
@@ -24,6 +24,11 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mehdi", "French")
+		want := "Bonjour, Mehdi"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
