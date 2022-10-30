2996235bc32747924e93a62dfb73182909e9cd88
Lucas Martin <lucasmartin045@gmail.com>
2022-10-30 22:41:44 +0800

    adding test case for spanish

--- hello_test.go
+++ hello_test.go
@@ -4,19 +4,31 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to Lucas", func(t *testing.T) {
-		got := Hello("Lucas")
+		got := Hello("Lucas", "")
 		want := "Hello, Lucas"
 
 		if got != want {
-			t.Errorf("got %q want %q", got, want)
+			assertCorrectMessage(t, got, want)
 		}
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		if got != want {
-			t.Errorf("got %q want %q", got, want)
+			assertCorrectMessage(t, got, want)
 		}
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
